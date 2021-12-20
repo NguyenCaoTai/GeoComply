57a59d2954863cc536dd194254cadd6e3060dc5a
tai nc <tai.nguyen@tyme.com>
2021-12-20 12:19:02 +0700

    + Adding UI layout

--- app/src/main/java/com/sfg/geocomply/ui/main/MainViewModel.java
+++ app/src/main/java/com/sfg/geocomply/ui/main/MainViewModel.java
@@ -1,7 +1,12 @@
 package com.sfg.geocomply.ui.main;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    public final MutableLiveData<String> comment = new MutableLiveData<>("");
+    public final LiveData<String> mentionResponse = new MutableLiveData<>("");
+    public final LiveData<String> linkResponse = new MutableLiveData<>("");
 }
